1a1da65b48da2fdc070e5c1995760640a7868669
Rayyanz <only4fcl@gmail.com>
2017-05-22 12:20:20 +0500

    Create learning.js

---
+++ learning.js
@@ -0,0 +1,13 @@
+// this is how you can covert a number into binary and them return the representation of binary as if you are looking for the representation of 2 it is 1 as its shown below.
+var countBits = function(n) {
+  // Program Me
+
+var result = n.toString(2); //Converts to binary
+var count = result.split(1);//  count -1 is your answer
+return((result.split('1')));
+
+};
+
+countBits(2), 2;
+
+
